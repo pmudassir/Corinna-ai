2fef621170e23fc40fde3e4a22966fd72f6cdc50
pmudassir <pmuddasir@gmail.com>
2024-07-20 19:38:09 +0300

    password change hooks v2

--- src/components/settings/change-password.tsx
+++ src/components/settings/change-password.tsx
@@ -1,8 +1,10 @@
+import { useChangePassword } from "@/hooks/settings/use-settings";
 import React from "react";
 
 type Props = {};
 
 const ChangePassword = (props: Props) => {
+  const { onChangePassword, register, errors, loading } = useChangePassword();
   return <div>ChangePassword</div>;
 };
 
